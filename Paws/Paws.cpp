ded59df839a8c5f8b25dabfe45b29d57dad28c4a
Steffen <46659645+german-one@users.noreply.github.com>
2019-06-16 21:38:49 +0200

    Fix for various bugs and issues

--- Paws/Paws.cpp
+++ Paws/Paws.cpp
@@ -3,133 +3,170 @@
 
 #include "pch.h"
 #include <iostream>
-#include "Paws.h"
 #include <string>
 #include <Windows.h>
 #include <conio.h>
 
-using namespace std;
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING ((DWORD)0x4)
+#endif
 
-enum Flags { Quiet, Verbose };
+struct VTColors
+{
+	std::wstring strVTReset{ L"\x1b[m" };
+	std::wstring strVTYellow{ L"\x1b[33m" };
+	std::wstring strVTGreen{ L"\x1b[32m" };
+	std::wstring strVTRed{ L"\x1b[31m" };
+
+	void clear()
+	{
+		strVTReset.clear();
+		strVTYellow.clear();
+		strVTGreen.clear();
+		strVTRed.clear();
+	}
+};
 
 // Forward Declarations:
-void DisplayUsage(bool fVTEnabled);
-void PauseBeforeClosing();
+void DisplayUsage(const VTColors& colors);
+void PauseBeforeClosing(const VTColors& colors);
 bool EnableVTProcessing();
-wstring GetErrorMessage();
+std::wstring GetErrorMessage(DWORD dwErrorCode);
+LPWSTR GetCmdLnSkipFirstToken();
 
-// Globals:
-wstring szVTReset{};
-wstring szVTYellow{};
-wstring szVTGreen{};
-wstring szVTRed{};
-
-int wmain(int argc, wchar_t* argv[])
+int main()
 {
+	DWORD dwExitCode{};
+	VTColors colors{};
+
 	// Enable Console VT Processing
 	bool fVTEnabled = EnableVTProcessing();
 
-	// Configure color strings if VT is enabled
-	if (fVTEnabled)
-	{
-		szVTReset.assign(L"\x1b[m");
-		szVTYellow.assign(L"\x1b[33m");
-		szVTGreen.assign(L"\x1b[32m");
-		szVTRed.assign(L"\x1b[31m");
-	}
+	// Configure color strings if VT is disabled
+	if (fVTEnabled == false)
+		colors.clear();
 
-	if (argc < 2)
-	{
-		DisplayUsage(fVTEnabled);
-	}
+	// Extract the command-line without the name of this executable:
+	LPWSTR szCommandLine{ GetCmdLnSkipFirstToken() };
+
+	if (szCommandLine == nullptr)
+		DisplayUsage(colors);
 	else
 	{
-		// Extract the command-line sans the name of this executable:
-		wstring strFullCommandLine{ (GetCommandLine()) };
-		wstring strThisExe{ argv[0] };
-		strThisExe.append(L" ");
-		size_t nStart{ strThisExe.length() + 2 };
-		size_t nLength{ strFullCommandLine.length() - strThisExe.length() - 1 };
-		wstring strCommandLine{ strFullCommandLine.substr(nStart, nLength) };
-
 		// Launch the requested command
-		wcout << szVTYellow << "Paws is executing: '" << szVTGreen << strCommandLine << szVTYellow << "'" << szVTReset << endl;
-		STARTUPINFO si{si.cb = sizeof(STARTUPINFO)};
+		std::wcout << colors.strVTYellow << L"Paws is executing: '" << colors.strVTGreen << szCommandLine << colors.strVTYellow << L'\'' << colors.strVTReset << std::endl;
+		STARTUPINFOW si{ sizeof(STARTUPINFOW) };
 		PROCESS_INFORMATION pi{};
-		if (CreateProcess(
-			NULL,
-			const_cast<LPWSTR>(strCommandLine.c_str()),
-			NULL,
-			NULL,
-			TRUE,
-			0,
-			NULL,
-			NULL,
-			&si,
-			&pi))
+		if (
+			CreateProcessW(
+				nullptr,
+				szCommandLine,
+				nullptr,
+				nullptr,
+				TRUE,
+				0,
+				nullptr,
+				nullptr,
+				&si,
+				&pi
+			) != FALSE)
 		{
 			// Wait for the executable to complete
 			WaitForSingleObject(pi.hProcess, INFINITE);
+			GetExitCodeProcess(pi.hProcess, &dwExitCode);
+			CloseHandle(pi.hThread);
+			CloseHandle(pi.hProcess);
 		}
 		else
 		{
-			wcout << szVTRed << L"Error: Paws failed to execute:" << GetErrorMessage() << szVTReset << endl;
+			dwExitCode = GetLastError();
+			std::wcerr << colors.strVTRed << L"Error: Paws failed to execute: " << GetErrorMessage(dwExitCode) << colors.strVTReset << std::endl;
 		}
 	}
 
 	//	Wait until the user hits a key before exiting
-	PauseBeforeClosing();
+	PauseBeforeClosing(colors);
+	return static_cast<int>(dwExitCode);
 }
 
-void PauseBeforeClosing()
+void PauseBeforeClosing(const VTColors& colors)
 {
-	EnableVTProcessing();
-	wcout << endl;
-	wcout << szVTYellow << "Paws-ed: Press any key to exit ..." << szVTReset << endl;
-	while (0 == _kbhit());
+	std::wcout << colors.strVTYellow << L"\nPaws-ed: Press any key to exit . . . " << colors.strVTReset << std::flush;
+	static_cast<void>(_getwch());
+	std::wcout << std::endl;
 }
 
-void DisplayUsage(bool fVTEnabled)
+void DisplayUsage(const VTColors& colors)
 {
-	wcout << szVTYellow << "Paws" << szVTReset << ": Runs the specified command, and then pauses before closing" << endl;
-	wcout << "Usage: " << szVTYellow << "Paws" << szVTReset << "[.exe] <" << szVTGreen << "command"<< szVTReset << "> <" << szVTGreen << "args" << szVTReset << ">" << endl;
-	wcout << endl;
-	wcout << "    command : Command to be run. If an executable is specified, it must be reachable on the current path" << endl;
-	wcout << "    args    : Arguments to be passed to the executable" << endl;
+	std::wcout << colors.strVTYellow << L"Paws" << colors.strVTReset << L": Runs the specified command, and then pauses before closing\n"
+		L"Usage: " << colors.strVTYellow << L"Paws" << colors.strVTReset << L"[.exe] <" << colors.strVTGreen << L"command" << colors.strVTReset << L"> <" << colors.strVTGreen << L"args" << colors.strVTReset << L">\n\n"
+		L"    command : Command to be run. If an executable is specified, it must be reachable on the current path\n"
+		L"    args    : Arguments to be passed to the executable" << std::endl;
 }
 
-wstring GetErrorMessage()
+std::wstring GetErrorMessage(DWORD dwErrorCode)
 {
-	// Retrieve the system error message for the last-error code
-
 	LPWSTR lpMsgBuf{};
-	DWORD dwErrorCode{ GetLastError() };
-
-	FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dwErrorCode,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		lpMsgBuf,
-		0, NULL);
-
-	wstring msg{ lpMsgBuf };
+	std::wstring msg{};
+	if (
+		FormatMessageW(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			nullptr,
+			dwErrorCode,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			reinterpret_cast<LPWSTR>(&lpMsgBuf),
+			0,
+			nullptr
+		) != 0)
+	{
+		msg.assign(lpMsgBuf);
+	}
 
 	LocalFree(lpMsgBuf);
-
 	return msg;
 }
 
 bool EnableVTProcessing()
 {
 	DWORD consoleMode{};
-	HRESULT hr{ E_UNEXPECTED };
 	HANDLE hConsole = { GetStdHandle(STD_OUTPUT_HANDLE) };
-	GetConsoleMode(hConsole, &consoleMode);
-	hr = SetConsoleMode(hConsole, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)
-		? S_OK
-		: GetLastError();
-	return S_OK == hr;
+	return (GetConsoleMode(hConsole, &consoleMode) != FALSE &&
+		SetConsoleMode(hConsole, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING) != FALSE);
+}
+
+LPWSTR GetCmdLnSkipFirstToken()
+{
+	static LPWSTR cmdLn{};
+	bool isInQuotes{};
+	bool isInToken{};
+	DWORD tokensCnt{};
+
+	if (cmdLn == nullptr)
+		cmdLn = GetCommandLineW();
+
+	for (LPWCH cursor{ cmdLn }; *cursor != L'\0'; ++cursor)
+	{
+		switch (*cursor)
+		{
+		case L' ': case L'\t': // Token separators, unless in a quoted substring
+			if (isInQuotes == false)
+				isInToken = false;
+			break;
+
+		case L'\"':
+			isInQuotes = !isInQuotes;
+		default: // This includes quotation marks
+			if (isInToken == false)
+			{
+				++tokensCnt;
+				if (tokensCnt > 1) // That is, the cursor points to the beginning of the second token
+					return cursor;
+
+				isInToken = true;
+			}
+			break;
+		}
+	}
+
+	return nullptr;
 }
